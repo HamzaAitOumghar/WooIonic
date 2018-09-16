4e4ef030d6a8fcc4f7aa8d49da55fadbecd981dd
HAMZA AIT OUMGHAR <hamwac5@yahoo.fr>
2018-09-16 16:46:46 +0100

    Finished Home Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,60 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  WooCommerce: any;
+  products:any[];
+  page:number;
+  moreProducts:any[];
 
+
+  constructor(public navCtrl: NavController,public toast : ToastController) {
+    this.page=2;  
+    this.WooCommerce=WC({
+        url:"http://localhost/wordpress",
+        consumerKey:"ck_856a139df3131d377fdb4dac1c11c23e44c771b3",
+        consumerSecret:"cs_85ce234c3245036db5c6dfddb029d3008b5fc9be",
+        wpAPI: true,
+        version: "wc/v2"
+      });
+      this.WooCommerce.getAsync("products").then(
+        (data)=>{
+          this.products=JSON.parse(data.body);
+          console.log(this.products);
+         },(err)=>{
+          console.log(err);
+        }
+      );
+      this.loadMoreProducts(null);
+  }
+
+  loadMoreProducts(event){
+
+    if(event==null){
+        this.page=2;
+        this.moreProducts=[];
+    }
+    else{
+      this.page++;
+    }
+
+    this.WooCommerce.getAsync("products?page="+this.page).then(
+      (data)=>{
+        this.moreProducts=this.moreProducts.concat(JSON.parse(data.body));
+        if(event !=null){
+          event.complete();
+        }
+
+        if(JSON.parse(data.body).length<10){
+          event.enable(false);
+          this.toast.create({
+              message: "No More Products !",
+              duration : 2000
+            }).present();
+        }
+      },(err)=>{
+        console.log(err);
+      }
+    );
   }
 
 }
